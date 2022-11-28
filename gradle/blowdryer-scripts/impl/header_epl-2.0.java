3a5158a9eedb52f02226586ff2e283d787313209
Ned Twigg <ned.twigg@diffplug.com>
2022-11-27 23:48:41 -0800

    Put license headers on all sourcecode.

--- gradle/blowdryer-scripts/impl/header_epl-2.0.java
+++ gradle/blowdryer-scripts/impl/header_epl-2.0.java
@@ -11,16 +11,3 @@
  * Contributors:
  *     EquoTech, Inc. - initial API and implementation
  *******************************************************************************/
-package dev.equo.ide.gradle;
-
-import org.gradle.api.DefaultTask;
-import org.gradle.api.tasks.TaskAction;
-
-public class EquoIdeTask extends DefaultTask {
-	EquoIdeExtension extension;
-
-	@TaskAction
-	public void launch() {
-		System.out.println("DO THE LAUNCH");
-	}
-}
--- plugin-gradle/src/main/java/dev/equo/ide/gradle/EquoIdeGradlePlugin.java
+++ plugin-gradle/src/main/java/dev/equo/ide/gradle/EquoIdeGradlePlugin.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2022 EquoTech, Inc. and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     EquoTech, Inc. - initial API and implementation
+ *******************************************************************************/
 package dev.equo.ide.gradle;
 
 import org.gradle.api.Plugin;
