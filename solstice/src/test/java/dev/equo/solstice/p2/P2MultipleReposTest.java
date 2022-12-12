5d5309dbeb3c3588f2d9e10c46121eea75e048d4
Ned Twigg <ned.twigg@diffplug.com>
2022-12-12 00:21:21 -0800

    Setup a complicated provisioning case with lots of jars.

--- solstice/src/test/java/dev/equo/solstice/p2/P2MultipleReposTest.java
+++ solstice/src/test/java/dev/equo/solstice/p2/P2MultipleReposTest.java
@@ -24,8 +24,8 @@ public class P2MultipleReposTest {
 		var session = new P2Session();
 		try (var client = new P2Client()) {
 			session.populateFrom(client, "https://download.eclipse.org/eclipse/updates/4.26/");
-			session.populateFrom(
-					client, "https://download.eclipse.org/tools/cdt/releases/11.0/cdt-11.0.0/");
+			session.populateFrom(client, "https://download.eclipse.org/tools/cdt/releases/11.0/cdt-11.0.0/");
+			session.populateFrom(client, "https://download.eclipse.org/buildship/updates/e423/releases/3.x/3.1.6.v20220511-1359/");
 		}
 		return session;
 	}
@@ -33,7 +33,7 @@ public class P2MultipleReposTest {
 	final ConsoleTable.Format format = ConsoleTable.Format.ASCII;
 
 	@Test
-	public void categories(Expect expect) throws Exception {
+	public void features(Expect expect) throws Exception {
 		var session = populateSession();
 		var query = session.query();
 		query.addAllUnits();
@@ -44,6 +44,12 @@ public class P2MultipleReposTest {
 	public void query(Expect expect) throws Exception {
 		var session = populateSession();
 		var query = session.query();
+		query.excludePrefix("tooling");
+		query.excludeSuffix(".source");
+		query.platformNone();
+		query.install("org.eclipse.platform.ide.categoryIU");
+		query.install("org.eclipse.releng.java.languages.categoryIU");
+		query.install("org.eclipse.buildship.feature.group");
 		query.install("202211300214.main");
 		var buffer = new StringBuilder();
 		buffer.append(ConsoleTable.ambiguousRequirements(query, format));
@@ -52,5 +58,6 @@ public class P2MultipleReposTest {
 		buffer.append('\n');
 		buffer.append(ConsoleTable.mavenStatus(query.getJars(), format));
 		buffer.append('\n');
+		expect.toMatchSnapshot(buffer);
 	}
 }
