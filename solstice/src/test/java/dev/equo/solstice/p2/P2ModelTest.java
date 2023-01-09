304f904cd3677efa0898a05c1a56ea56dc4c92e2
Ned Twigg <ned.twigg@diffplug.com>
2023-01-09 12:29:02 -0800

    Better way to test P2Model.toString()

--- solstice/src/test/java/dev/equo/solstice/p2/P2ModelTest.java
+++ solstice/src/test/java/dev/equo/solstice/p2/P2ModelTest.java
@@ -10,29 +10,19 @@ public class P2ModelTest {
     @Test
     public void toString(Expect expect) {
         P2Model model = new P2Model();
-        StringBuilder buf = new StringBuilder();
-        addToStringTest(buf, "EMPTY", model);
+        expect.scenario("empty").toMatchSnapshot(model.toString());
 
         model.getP2repo().add("https://download.eclipse.org/eclipse/updates/4.26/");
-        addToStringTest(buf, "P2 SINGLE", model);
+        expect.scenario("p2 single").toMatchSnapshot(model.toString());
 
         model.getP2repo().add("https://download.eclipse.org/buildship/updates/e423/releases/3.x/3.1.6.v20220511-1359/");
-        addToStringTest(buf, "P2 MULTIPLE", model);
+        expect.scenario("p2 multiple").toMatchSnapshot(model.toString());
 
         model.getInstall().add("org.eclipse.platform.ide.categoryIU");
-        addToStringTest(buf, "INSTALL SINGLE", model);
+        expect.scenario("install single").toMatchSnapshot(model.toString());
 
         model.getInstall().add("org.eclipse.releng.java.languages.categoryIU");
         model.getInstall().add("org.eclipse.buildship.feature.group");
-        addToStringTest(buf, "INSTALL MULTIPLE", model);
-
-        expect.toMatchSnapshot(buf.toString().trim());
-    }
-
-    private void addToStringTest(StringBuilder buf, String name, P2Model model) {
-        buf.append(name);
-        buf.append('\n');
-        buf.append(model.toString());
-        buf.append('\n');
+        expect.scenario("install multiple").toMatchSnapshot(model.toString());
     }
 }
