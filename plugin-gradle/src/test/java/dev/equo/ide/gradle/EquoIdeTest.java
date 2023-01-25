db20658ff302d6a6456dd63d9025bfd157cc9552
Ned Twigg <ned.twigg@diffplug.com>
2023-01-25 03:57:33 -0800

    Hook it into the Gradle DSL with a usage example.

--- plugin-gradle/src/test/java/dev/equo/ide/gradle/EquoIdeTest.java
+++ plugin-gradle/src/test/java/dev/equo/ide/gradle/EquoIdeTest.java
@@ -16,7 +16,6 @@ package dev.equo.ide.gradle;
 import au.com.origin.snapshots.Expect;
 import au.com.origin.snapshots.junit5.SnapshotExtension;
 import java.io.IOException;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
@@ -99,14 +98,14 @@ public class EquoIdeTest extends GradleHarness {
 	}
 
 	@Test
-	@Disabled
+	// @Disabled
 	public void equoIde() throws IOException {
 		setFile("build.gradle")
 				.toLines(
 						"plugins { id 'dev.equo.ide' }",
 						"equoIde {",
-						"}",
-						"// (placeholder so GPJ formats this nicely)");
+						"  welcome().openUrlOnStartup('https://github.com/equodev/equo-ide')",
+						"}");
 		gradleRunner()
 				.withArguments("equoIde", "--show-console", "--stacktrace")
 				.forwardOutput()
