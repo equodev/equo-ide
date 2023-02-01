fe77a4c3978112fc4320dc747bc3cbd1550432f0
Ned Twigg <ned.twigg@diffplug.com>
2023-01-31 23:22:04 -0800

    SolsticeManifestTest.junitPlatformCommons

--- solstice/src/test/java/dev/equo/solstice/SolsticeManifestTest.java
+++ solstice/src/test/java/dev/equo/solstice/SolsticeManifestTest.java
@@ -15,46 +15,31 @@ package dev.equo.solstice;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.osgi.framework.BundleException;
 
 public class SolsticeManifestTest {
 	@Test
-	public void testManifestStrip() throws BundleException {
+	public void junitPlatformCommons() {
 		var orig =
-				("org.eclipse.equinox.internal.p2.persistence; x-friends:=\n"
-								+ " \"org.eclipse.equinox.p2.artifact.repository,  org.eclipse.equinox.p2.en\n"
-								+ " gine,  org.eclipse.equinox.p2.metadata.repository,  org.eclipse.equinox\n"
-								+ " .p2.ui.importexport,  org.eclipse.equinox.p2.repository.tools\",org.ecli\n"
-								+ " pse.equinox.internal.p2.repository; x-friends:=\"org.eclipse.equinox.p2.\n"
-								+ " artifact.repository,  org.eclipse.equinox.p2.metadata.repository,  org.\n"
-								+ " eclipse.equinox.p2.updatesite,  org.eclipse.equinox.p2.repository.tools\n"
-								+ " ,  org.eclipse.equinox.p2.transport.ecf,  org.eclipse.equinox.p2.engine\n"
-								+ " ,  org.eclipse.equinox.p2.discovery.compatibility,  org.eclipse.equinox\n"
-								+ " .p2.publisher\",org.eclipse.equinox.internal.p2.repository.helpers; x-fr\n"
-								+ " iends:=\"org.eclipse.equinox.p2.artifact.repository,  org.eclipse.equino\n"
-								+ " x.p2.exemplarysetup,  org.eclipse.equinox.p2.metadata.repository,  org.\n"
-								+ " eclipse.equinox.p2.operations,  org.eclipse.equinox.p2.publisher,  org.\n"
-								+ " eclipse.equinox.p2.repository.tools,  org.eclipse.equinox.p2.ui,  org.e\n"
-								+ " clipse.equinox.p2.updatesite\",org.eclipse.equinox.internal.provisional.\n"
-								+ " p2.repository,org.eclipse.equinox.p2.repository;version=\"2.0.0\",org.ecl\n"
-								+ " ipse.equinox.p2.repository.artifact;version=\"2.3.0\",org.eclipse.equinox\n"
-								+ " .p2.repository.artifact.spi;version=\"2.0.0\",org.eclipse.equinox.p2.repo\n"
-								+ " sitory.metadata;version=\"2.0.0\",org.eclipse.equinox.p2.repository.metad\n"
-								+ " ata.spi;version=\"2.0.0\",org.eclipse.equinox.p2.repository.spi;version=\"\n"
-								+ " 2.0.0\"")
+				("org.junit.platform.commons;version=\"1.9.1\";status=STABLE\n"
+								+ " ;uses:=\"org.apiguardian.api,org.junit.platform.commons.util\",org.junit.\n"
+								+ " platform.commons.annotation;version=\"1.9.1\";status=STABLE;uses:=\"org.ap\n"
+								+ " iguardian.api\",org.junit.platform.commons.function;version=\"1.9.1\";stat\n"
+								+ " us=MAINTAINED;uses:=\"org.apiguardian.api\",org.junit.platform.commons.lo\n"
+								+ " gging;version=\"1.9.1\";status=INTERNAL;mandatory:=status;uses:=\"org.apig\n"
+								+ " uardian.api\",org.junit.platform.commons.support;version=\"1.9.1\";status=\n"
+								+ " MAINTAINED;uses:=\"org.apiguardian.api,org.junit.platform.commons,org.ju\n"
+								+ " nit.platform.commons.function\",org.junit.platform.commons.util;version=\n"
+								+ " \"1.9.1\";status=DEPRECATED;uses:=\"org.apiguardian.api,org.junit.platform\n"
+								+ " .commons,org.junit.platform.commons.function\"")
 						.replace("\n ", "");
 		var headers = SolsticeManifest.parseAndStripManifestHeader(orig);
 		Assertions.assertThat(headers)
 				.containsExactly(
-						"org.eclipse.equinox.internal.p2.persistence",
-						"org.eclipse.equinox.internal.p2.repository",
-						"org.eclipse.equinox.internal.p2.repository.helpers",
-						"org.eclipse.equinox.internal.provisional.p2.repository",
-						"org.eclipse.equinox.p2.repository",
-						"org.eclipse.equinox.p2.repository.artifact",
-						"org.eclipse.equinox.p2.repository.artifact.spi",
-						"org.eclipse.equinox.p2.repository.metadata",
-						"org.eclipse.equinox.p2.repository.metadata.spi",
-						"org.eclipse.equinox.p2.repository.spi");
+						"org.junit.platform.commons",
+						"org.junit.platform.commons.annotation",
+						"org.junit.platform.commons.function",
+						"org.junit.platform.commons.logging",
+						"org.junit.platform.commons.support",
+						"org.junit.platform.commons.util");
 	}
 }
