a71a1959bb68296485aefc5a459305b254eb052b
Ned Twigg <ned.twigg@diffplug.com>
2023-03-13 14:12:03 -0700

    Set a property so that gosh shell stops yelling about motd

--- solstice/src/main/java/dev/equo/ide/BuildPluginIdeMain.java
+++ solstice/src/main/java/dev/equo/ide/BuildPluginIdeMain.java
@@ -273,6 +273,7 @@ public class BuildPluginIdeMain {
 		}
 
 		var props = new LinkedHashMap<String, String>();
+		props.put("gosh.args", "--quiet --noshutdown");
 		props.put(Constants.FRAMEWORK_STORAGE_CLEAN, Constants.FRAMEWORK_STORAGE_CLEAN_ONFIRSTINIT);
 		props.put(
 				EquinoxLocations.PROP_INSTANCE_AREA, new File(installDir, "instance").getAbsolutePath());
