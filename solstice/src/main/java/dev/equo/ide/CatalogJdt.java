d3d699ea3cb9794ac5d5ce0bf158e2d3b368028d
Ned Twigg <ned.twigg@diffplug.com>
2023-05-09 11:07:49 -0700

    Prepare for preferences DSL for JDT and Platform.

---
+++ solstice/src/main/java/dev/equo/ide/CatalogJdt.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2022 EquoTech, Inc. and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     EquoTech, Inc. - initial API and implementation
+ *******************************************************************************/
+package dev.equo.ide;
+
+import java.util.List;
+
+public class CatalogJdt extends Catalog {
+	CatalogJdt() {
+		super("jdt", PLATFORM, List.of("org.eclipse.releng.java.languages.categoryIU"), PLATFORM);
+	}
+}
