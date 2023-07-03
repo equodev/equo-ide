5070dd8e4dbcbaf71b154d2437ecc06a61cb6ae9
Ned Twigg <ned.twigg@diffplug.com>
2023-07-03 10:29:39 -0700

    Make CatalogTest more future-proof.

--- solstice/src/test/java/dev/equo/solstice/p2/CatalogTest.java
+++ solstice/src/test/java/dev/equo/solstice/p2/CatalogTest.java
@@ -1,3 +1,16 @@
+/*******************************************************************************
+ * Copyright (c) 2023 EquoTech, Inc. and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *     EquoTech, Inc. - initial API and implementation
+ *******************************************************************************/
 package dev.equo.solstice.p2;
 
 import au.com.origin.snapshots.Expect;
@@ -5,38 +18,37 @@ import au.com.origin.snapshots.junit5.SnapshotExtension;
 import dev.equo.ide.Catalog;
 import dev.equo.ide.CatalogDsl;
 import dev.equo.ide.IdeHook;
-import java.util.Map;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 
 @ExtendWith({SnapshotExtension.class})
 public class CatalogTest {
-    @Test
-    public void equoChatGptDefault(Expect expect) {
-        expect.toMatchSnapshot(requestModel(new TestCatalogDsl(Catalog.CHATGPT, null)));
-    }
+	@Test
+	public void equoChatGptDefault(Expect expect) {
+		expect.toMatchSnapshot(requestModel(new TestCatalogDsl(Catalog.CHATGPT, null)));
+	}
 
-    @Test
-    public void equoChatGptFixed(Expect expect) {
-        expect.toMatchSnapshot(requestModel(new TestCatalogDsl(Catalog.CHATGPT, "1.0.0")));
-    }
+	@Test
+	public void equoChatGptFixed(Expect expect) {
+		expect.toMatchSnapshot(requestModel(new TestCatalogDsl(Catalog.CHATGPT, "9.9.9")));
+	}
 
-    static class TestCatalogDsl extends CatalogDsl {
-        public TestCatalogDsl(Catalog catalog, String urlOverride) {
-            super(catalog);
-            super.setUrlOverride(urlOverride);
-        }
-    }
+	static class TestCatalogDsl extends CatalogDsl {
+		public TestCatalogDsl(Catalog catalog, String urlOverride) {
+			super(catalog);
+			super.setUrlOverride(urlOverride);
+		}
+	}
 
-    private String requestModel(TestCatalogDsl... catalogs) {
-        var list = new CatalogDsl.TransitiveAwareList<TestCatalogDsl>();
-        for (var catalog : catalogs) {
-            list.add(catalog);
-        }
-        var model = new P2Model();
-        var hooks = new IdeHook.List();
-        list.putInto(model, hooks);
+	private String requestModel(TestCatalogDsl... catalogs) {
+		var list = new CatalogDsl.TransitiveAwareList<TestCatalogDsl>();
+		for (var catalog : catalogs) {
+			list.add(catalog);
+		}
+		var model = new P2Model();
+		var hooks = new IdeHook.List();
+		list.putInto(model, hooks);
 
-        return ConsoleTable.request(model, ConsoleTable.Format.ascii);
-    }
+		return ConsoleTable.request(model, ConsoleTable.Format.ascii);
+	}
 }
